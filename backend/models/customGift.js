10bee99d1469862d91cf5fbc0a683f92ea94d2f8
Sapoorna <sapoornajanani@gmail.com>
2026-04-02 23:39:25 +0530

    Fix CustomGift model fields

--- backend/models/customGift.js
+++ backend/models/customGift.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const customGiftSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String },
-  description: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('CustomGift', customGiftSchema);
